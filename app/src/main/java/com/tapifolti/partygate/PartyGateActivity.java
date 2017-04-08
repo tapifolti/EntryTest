b89811a82d2b0760487b595b1c0e2a181eab71f8
tapifolti <susanterge@hotmail.com>
2017-04-08 20:36:20 +0200

    test

--- app/src/main/java/com/tapifolti/partygate/PartyGateActivity.java
+++ app/src/main/java/com/tapifolti/partygate/PartyGateActivity.java
@@ -13,6 +13,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+//
 public class PartyGateActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
