04b7aabb4d50065683d835d65cfd0e19f3bbaaa4
tapifolti <susanterge@hotmail.com>
2017-04-09 20:22:49 +0200

    app layout, 3 activities, common camera fragement, built but not started

---
+++ app/src/main/java/com/tapifolti/partygate/RegisterActivity.java
@@ -0,0 +1,23 @@
+package com.tapifolti.partygate;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by anya on 3/31/2017.
+ */
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        if (null == savedInstanceState) {
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.container, PhotoFragment.newInstance())
+                    .commit();
+        }
+    }
+
+}
